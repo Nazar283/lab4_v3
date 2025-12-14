a0caa1e84d88855f6933542135947996284c9c8b
Nazar283 <nazar.litkevych.ir.2023@lpnu.ua>
2025-12-14 15:20:35 +0200

    сome back

--- app.py
+++ app.py
@@ -48,13 +48,13 @@ swagger = Swagger(app, config=swagger_config)
 # --- End Swagger Config ---
 
 # --- Реєстрація Blueprint'ів ---
-# app.register_blueprint(app_bp, url_prefix='/api/apps')
-# app.register_blueprint(user_bp, url_prefix='/api/users')
-# app.register_blueprint(developer_bp, url_prefix='/api/developers')
-# app.register_blueprint(category_bp, url_prefix='/api/categories')
-# app.register_blueprint(review_bp, url_prefix='/api/reviews')
-# app.register_blueprint(installation_bp, url_prefix='/api/installations')
-# app.register_blueprint(country_bp, url_prefix='/api/countries')
+app.register_blueprint(app_bp, url_prefix='/api/apps')
+app.register_blueprint(user_bp, url_prefix='/api/users')
+app.register_blueprint(developer_bp, url_prefix='/api/developers')
+app.register_blueprint(category_bp, url_prefix='/api/categories')
+app.register_blueprint(review_bp, url_prefix='/api/reviews')
+app.register_blueprint(installation_bp, url_prefix='/api/installations')
+app.register_blueprint(country_bp, url_prefix='/api/countries')
 # --- Кінець реєстрації ---
 
 if __name__ == '__main__':
